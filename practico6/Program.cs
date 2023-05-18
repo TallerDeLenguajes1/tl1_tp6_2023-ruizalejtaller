bda2c6de131270ad43c9a65a2e03cd0f789bb79f
Ramón Alejandro Ruiz <alexestelar@yahoo.com.ar>
2023-05-18 17:37:49 -0300

    Ejercicio 1

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+/*
 Console.WriteLine("Hello, World!");
 
 int a;
@@ -9,4 +11,22 @@ b=a;
 
 Console.WriteLine("Valor de a: "+a);
 Console.WriteLine("Valor de b: "+b);
+*/
 
+String str = "";
+float num;
+
+Console.WriteLine("Escriba un número: ");
+
+str = Console.ReadLine();
+
+bool anda = float.TryParse(str, out num);
+
+
+if (anda)
+{
+    if (num>0)
+    {
+    Console.WriteLine("El inverso de: "+num+" es 1/"+num);
+    } else Console.WriteLine(num+ " es menor que cero");
+}
